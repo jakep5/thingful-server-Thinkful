9dc4b4ff1217bbb69b91f4d24d7ede7524a5ef42
jakep5 <jakepagel1@yahoo.com>
2019-11-21 14:17:01 -0600

    fix bcrypt bug

--- src/users/users-service.js
+++ src/users/users-service.js
@@ -1,6 +1,7 @@
 const REGEX_UPPER_LOWER_NUMBER_SPECIAL = /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&])[\S]+/
 
 const xss = require('xss')
+const bcrypt = require('bcryptjs')
 
 const UsersService = {
 
