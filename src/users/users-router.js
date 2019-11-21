13f88ad870246c18f507883891896e937a8683d2
jakep5 <jakepagel1@yahoo.com>
2019-11-21 14:21:43 -0600

    fix nickname typo again

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -7,7 +7,7 @@ const jsonBodyParser = express.json()
 
 usersRouter
     .post('/', jsonBodyParser, (req, res, next) => {
-        const { password, user_name, full_name, nick_name } = req.body
+        const { password, user_name, full_name, nickname } = req.body
 
         for (const field of ['full_name', 'user_name', 'password'])
             if(!req.body[field])
@@ -36,7 +36,7 @@ usersRouter
                             user_name,
                             password: hashedPassword,
                             full_name,
-                            nick_name,
+                            nickname,
                             date_created: 'now()',
                         }
 
